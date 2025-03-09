649badfd051de28c0b1e0f0905ac96493e2f0941
vikash990 <vikashadubey@gmail.com>
2025-03-09 19:54:15 +0530

    new website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import SubHeader from './components/SubHeader';
 import Footer from './components/Footer';
 
 // Page Components
